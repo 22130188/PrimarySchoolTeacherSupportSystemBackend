eace73bc68de3dcd6df23d139895bcf5633f2f7b
22130188 <22130188@st.hcmuaf.edu.vn>
2026-02-25 20:57:37 +0700

    create new project

---
+++ src/main/java/vn/edu/primary/teacher_support/PrimarySchoolTeacherSupportSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.primary.teacher_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimarySchoolTeacherSupportSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimarySchoolTeacherSupportSystemBackendApplication.class, args);
+	}
+
+}
